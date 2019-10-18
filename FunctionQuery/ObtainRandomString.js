e01bbfee94b19d1158724685624bf3921f8eee13
sharksb <46184626+sharksb@users.noreply.github.com>
2019-10-18 13:15:43 +0800

    Create ObtainRandomString.js

---
+++ FunctionQuery/ObtainRandomString.js
@@ -0,0 +1,26 @@
+/**
+ * e 表示长度，默认32位
+ */
+First:
+function randomString(e) {    
+    e = e || 32;
+    var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",
+    a = t.length,
+    n = "";
+    for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
+    return n
+}
+
+Second:
+ function randomString(len) {
+        len = len || 32;
+        var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+        var maxPos = $chars.length;
+        var pwd = '';
+        for (i = 0; i < len; i++) {
+            pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+        }
+        return pwd;
+    }
+
+note:obtain random string
